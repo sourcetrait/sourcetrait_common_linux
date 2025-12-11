155862d4a4c9e21520cec1594615ea9d10db2fd7
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-11 14:35:57 -0800

    linux plat

---
+++ crates/sourcetrait_crossplat_linux/src/lib.rs
@@ -0,0 +1,41 @@
+#[cfg(feature = "crossplat")]
+pub(crate) mod crossplat {
+    pub(crate) mod component {
+        pub(crate) mod net {
+            pub(crate) mod net;
+            //pub(crate) mod nss;
+        }
+        pub(crate) mod cmd;
+        pub(crate) mod ui;
+    }
+    pub(crate) mod consts;
+    //pub(crate) mod error;
+    //pub(crate) mod model;
+}
+
+pub use crate::{
+    crossplat::{
+        component::{
+            net::{
+                net::*,
+                //nss::*,
+            },
+            cmd::*,
+            ui::*,
+        },
+        consts::*,
+    },
+};
+
+pub(crate) use std::{
+    env,
+    path::{Path, PathBuf},
+    process::Command,
+};
+
+pub(crate) use sourcetrait_twostr::*;
+pub(crate) use sourcetrait_crossplat_bridge::{
+    self as cross,
+    prelude::driver::*
+};
+pub(crate) use sourcetrait_crossplat_unix as unix;
