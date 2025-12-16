aabf00b88f5287363e1f60748d980c8435416814
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-12-16 08:11:56 -0800

    fixed net

--- crates/sourcetrait_crossplat_linux/src/crossplat/component/net/net.rs
+++ crates/sourcetrait_crossplat_linux/src/crossplat/component/net/net.rs
@@ -6,13 +6,13 @@ impl cross::NetComponentLookup for LinuxNetComponentLookup {
         unix::lookup_hostname()
     }
     
-    fn lookup_domain(&self) -> cross::BridgeResult<cross::Capable<Option<String>>> {
+    fn lookup_domain(&self) -> cross::BridgeResult<cross::Capable<cross::DomainsCapable, Option<String>>> {
         //let _nss_db = NssDatabase::lookup()?;
         //TODO: check parse sssd.conf
         cross::BridgeError::err_incapable(cross::Capability::Domains)
     }
 
-    fn lookup_domain_authorities(&self) -> cross::BridgeResult<Vec<cross::DomainAuthority>> {
+    fn lookup_domain_authorities(&self) -> cross::BridgeResult<cross::Capable<cross::DomainsCapable, Vec<cross::DomainAuthority>>> {
         cross::BridgeError::err_incapable(cross::Capability::Domains)
     }
 }
